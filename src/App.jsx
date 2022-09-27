f8b7cc1b19bd97c7a884afb834bd08f11315e48a
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-09-26 21:11:00 -0300

    feat: add component de grafico ao projeto style: estilização do grafico

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import './App.css'
-import Form from './components/Form/Form'
-import Header from './components/Header/Header'
-import Table from './components/Table/Table'
-import Tittle from './components/Tittle/Tittle'
+import "./App.css";
+import { Grafico } from "./components/Chart/Chart";
+import Form from "./components/Form/Form";
+import Header from "./components/Header/Header";
+import Table from "./components/Table/Table";
+import Tittle from "./components/Tittle/Tittle";
 
 function App() {
-
   return (
-    <div className="App">                                                                                                                                                                      
-      <Header/>
-      <Form/>
-      <Tittle/>
-      <Table/>
-      
+    <div className="App">
+      <Header />
+      <Form />
+      <Tittle />
+      <section className="dados">
+        <Table />
+        <Grafico />
+      </section>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,42 @@
+import React from "react";
 import { Chart } from "react-google-charts";
 
-<Chart
-  chartType="ScatterChart"
-  data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-  width="100%"
-  height="400px"
-  legendToggle
-/>
+export const data = [
+  ["Task", "Faixa etária"],
+  ["De 18 a 21 anos", 20],
+  ["De 22 a 25 anos", 20],
+  ["De 26 a 29 anos", 20],
+  ["De 30 a 33 anos", 20],
+  ["Acima de 33 anos", 20],
+];
+
+export const options = {
+  pieHole: 0.4,
+  is3D: false,
+  pieSliceText: 'none',
+  backgroundColor: '#FFDD00',
+  legend: {position: 'right', alignment: 'center', textStyle: { color: 'black',
+    fontName: "Archivo",
+    fontSize: 18,
+    bold: true,
+    italic: false}},
+  slices: {
+    0: { color: '#E56E25' },
+    1: { color: '#F18A1F' },
+    2: { color: '#3BA3D2' },
+    3: { color: '#4EB966' },
+    4: { color: '#EFB119' },
+  }
+};
+
+export function Grafico() {
+  return (
+    <Chart
+      chartType="PieChart"
+      width="100%"
+      height="500px"
+      data={data}
+      options={options}
+    />
+  );
+}
