49ce6c1c4041f19c3bc7594338a462f9cf17f7c4
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-10-03 03:52:43 -0300

    fix: resolução de um erro por conta de uma importação nula no main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import CadastroProvider from "./context/CadastroProvider";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
